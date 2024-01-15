bb7e3267239c4d1cf8fbac62dc27cea875a85bb9
Fabian Gander <fabian.gander@soulcode.ch>
2024-01-15 14:18:07 +0100

    Reporting now the last matched rule only for an extension. Improved logging output

--- src/extension.ts
+++ src/extension.ts
@@ -6,13 +6,13 @@ import { Configs } from './types';
 import { getExtensionsJson, isConfirm, showExtensionsInMarketplaceSearch } from './utils';
 import semver = require('semver');
 
-const logger = vscode.window.createOutputChannel("Unwanted recommendations");
+export const logger = vscode.window.createOutputChannel("Unwanted extensions");
 logger.show();
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	logger.appendLine('Extension "extensions-unwanted-recommendations" is now active!');
+	logger.appendLine('Extension "vscode-unwanted-extensions" is now active!');
 
 	// Check for extensions
 	checkingExtensions(context);
@@ -26,14 +26,18 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 async function checkingExtensions(context: ExtensionContext, verbose = false) {
+	logger.appendLine('');
+	logger.appendLine('###  Checking... ###');
+	logger.appendLine('');
+
 	// Get the extensions.json content
-	const configs: Configs = await getExtensionsJson(verbose);
+	const configuration: Configs = await getExtensionsJson(verbose);
 
 	// Make sure, that the unwantedRecommendations is defined, otherwise we have nothing to do
-	let amountOfUnwantedRecommendations = configs.unwantedRecommendations?.length ?? 0;
+	let amountOfUnwantedRecommendations = configuration.unwantedRecommendations?.length ?? 0;
 	if (amountOfUnwantedRecommendations === 0) {
-		logger.appendLine("No unwanted recommendations found.");
-		verbose && vscode.window.showWarningMessage("No unwanted recommendations found.");
+		logger.appendLine("No unwanted extensions found.");
+		verbose && vscode.window.showWarningMessage("No unwanted extensions found.");
 		return;
 	}
 
@@ -51,22 +55,24 @@ async function checkingExtensions(context: ExtensionContext, verbose = false) {
 			// Start checking for extensions
 			progress.report({ increment: 25, message: "checking... 🕵️" });
 
-			// Collect all unwanted extensions which are still enabled
-			let enabledUnwantedRecommendations: string[] = [];
 			const installedExtensions = vscode.extensions.all;
 			// Progress is already on 25, define the rest for every extension
 			let progressStep = 75 / amountOfUnwantedRecommendations;
+			
+			// Collect all unwanted extensions which are still enabled
+			const enabledUnwantedRecommendations: { [key: string]: string} = {};
 
 			// Iterate over the unwanted extensions
-			configs.unwantedRecommendations.map(async unwantedExtensionString => {
+			configuration.unwantedRecommendations.map(async unwantedExtensionString => {
+				logger.appendLine('');
 				progress.report({ increment: progressStep, message: unwantedExtensionString + "..." });
 
 				const [unwantedExtensionId, versionRange] = unwantedExtensionString.split("@");
-				logger.appendLine(unwantedExtensionId + "@" + versionRange);
+				logger.appendLine("### " + unwantedExtensionId + "@" + versionRange);
 
 				// Check if the extension is installed
 				const unwantedExtension = installedExtensions.find(extension => extension.id.toLowerCase() === unwantedExtensionId.toLowerCase());
-				logger.appendLine(`${unwantedExtensionId} is ${unwantedExtension ? "enabled" : "disabled"}`);
+				logger.appendLine(`Is ${unwantedExtension ? "enabled" : "disabled"}`);
 
 				// Check if the versionRange matches the version string, if there is a versionRange
 				let versionMatch = undefined;
@@ -83,31 +89,44 @@ async function checkingExtensions(context: ExtensionContext, verbose = false) {
 
 				// Extension must be enabled and if versionMatch exists it must be true (or undefined)
 				if (unwantedExtension && versionMatch !== false) {
-					enabledUnwantedRecommendations.push(unwantedExtensionId);
-
 					const messageUnwantedExtension = `Its recommended to disable "${unwantedExtension.packageJSON.displayName}" (${unwantedExtension.id}) for this workspace`;
 					const messageUnwantedExtensionVersion = `Its recommended to install another version of "${unwantedExtension.packageJSON.displayName}" for this workspace. Not recommended: ${unwantedExtension.id}@${versionRange}`;
 					const message = versionMatch ? messageUnwantedExtensionVersion : messageUnwantedExtension;
-					logger.appendLine(message);
 
-					// Allow the user to click a single extension to verify in the extension marketplace
-					const data = await vscode.window.showWarningMessage(message, { title: 'Show', value: 'show' },);
-					if (data?.value === "show") {
-						showExtensionsInMarketplaceSearch([unwantedExtension.id]);
-					}
+					// Add the unwanted extension to the list
+					enabledUnwantedRecommendations[unwantedExtensionId] = message;
+					logger.appendLine(message);
 				}
 			});
 
-			if (enabledUnwantedRecommendations.length > 0) {
+			// Go over all enabled unwanted extensions and show warnings
+			Object.entries(enabledUnwantedRecommendations).map(async ([unwantedExtensionId, message]) => {
+				// Allow the user to click a single extension to verify in the extension marketplace
+				const data = await vscode.window.showWarningMessage(message, { title: 'Show', value: 'show' },);
+				if (data?.value === "show") {
+					showExtensionsInMarketplaceSearch([unwantedExtensionId]);
+				}
+			});
+
+			const enabledUnwantedRecommendationIds = Object.keys(enabledUnwantedRecommendations);
+			const amountOfEnabledUnwantedRecommendations = enabledUnwantedRecommendationIds.length;
+			logger.appendLine(``);
+			logger.appendLine(`Summary:`);
+			logger.appendLine(`Project contains ${amountOfUnwantedRecommendations} unwanted extensions.`);
+
+			if (amountOfEnabledUnwantedRecommendations > 0) {
+				logger.appendLine(`${amountOfEnabledUnwantedRecommendations} are still enabled.`);
+
 				// Show a summary for the user about enabled unwanted extensions
-				progress.report({ increment: 100, message: `Found ${enabledUnwantedRecommendations.length} enabled unwanted extensions!` });
+				progress.report({ increment: 100, message: `Found ${amountOfEnabledUnwantedRecommendations} enabled unwanted extensions!` });
 				const result = await isConfirm("Display unwanted extensions?", "Do you want to display the unwanted extensions?\nIt's recommended to disable them for this workspace.\nYou need to do this manually.");
 				if (result) {
-					showExtensionsInMarketplaceSearch(enabledUnwantedRecommendations);
+					showExtensionsInMarketplaceSearch(enabledUnwantedRecommendationIds);
 				}
 			} else {
 				// Checked, all unwanted extensions are disabled
 				progress.report({ increment: 100, message: `Done ✔️` });
+				logger.appendLine(`All unwanted extensions are disabled.`);
 			}
 
 			// Wait a bit until we resolve the progress indicator Notification
