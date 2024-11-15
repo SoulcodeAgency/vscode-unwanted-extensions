50b7a3d608f80f220f446d2ba8e23531c7c83df8
Fabian Gander <fabian.gander@soulcode.ch>
2024-11-15 22:05:01 +0100

    Fix: Do not open the output channel automatically

--- src/extension.ts
+++ src/extension.ts
@@ -8,7 +8,6 @@ import semver = require('semver');
 import { checkUpdateNotification } from './update';
 
 export const logger = vscode.window.createOutputChannel("Unwanted extensions");
-logger.show();
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
